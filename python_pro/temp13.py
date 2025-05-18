1b593cb1dc3b1943359bbdb4c8320c85be8c5a4e
jian69-yunhai <fs_jian@139.com>
2025-05-18 17:47:29 +0800

    添加temp13.py文件

--- python_pro/temp13.py
+++ python_pro/temp13.py
@@ -1,2 +1,17 @@
-t = (12345,54321, 'hello!')
-print(t[0])
+#temp13.py
+import turtle
+def snow(n,size):
+    if n==0:
+        turtle.fd(size)
+    else:
+        for angle in [0,60,-120,60]:
+            turtle.left(angle)
+            snow(n-1,size/3)
+
+turtle.setup(800,600)
+turtle.penup()
+turtle.goto(-300,-50)
+turtle.pendown()
+turtle.pensize(3)
+snow(3,600)
+turtle.done()
